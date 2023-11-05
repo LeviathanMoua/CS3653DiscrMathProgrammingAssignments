1a76e8cff3118960aba83418b0b5030096325869
LeviathanMoua <39628304+LeviathanMoua@users.noreply.github.com>
2023-11-04 21:51:46 -0500

    Programming Assignment 3 | Upload #2
    Files included:
    - Instructions/Homework for PA3 SN1-SN4
    - PA3 SN1 (Completed)
    - PA2 SN2 (Completed but needs to be touched up in the code editor).
    
    
    Not Included:
    - .cpp for PA3SN3 (required)
    - .cpp for PA3SN4 (required)

--- PA3/MouaJustin_DiscrMath_PA3SN2.cpp
+++ PA3/MouaJustin_DiscrMath_PA3SN2.cpp
@@ -13,10 +13,11 @@ Description  : Given the positive integers a, b, and m with m > 1, find ab mod m
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 void fnDecimalToBinary(int intN) {
     //==============BINARY CONVERSION================
-    std::vector<int>nums; // list that will hold binary values
+    std::vector<int> vecBinary; // list that will hold binary values
 
 
     int i = 0; // iterator for vector
@@ -33,16 +34,18 @@ void fnDecimalToBinary(int intN) {
         //1%2 = 1. 1/2 then becomes 0.
         //Program stops since we have reached 0. Note that we do not
         //Have decimals since we are dealing with integer, not float!
-        nums.push_back(intN % 2); // adds binary value to the back of string 
+        vecBinary.push_back(intN % 2); // adds binary value to the back of string 
         i++; // i gets incremented for the next position in vector 
         intN = intN / 2;
 
     }
 
-    reverse(nums.begin(), nums.end()); // reverses order of vector 
+    reverse(vecBinary.begin(), vecBinary.end()); // reverses order of vector 
 
-    for (int i = 0; i < nums.size(); i++) {
-        std::cout << nums[i];
+    std::string strFinalResult;
+
+    for (int i = 0; i < vecBinary.size(); i++) {
+        std::cout << vecBinary[i];
     }
 
 
@@ -53,7 +56,7 @@ void fnDecimalToBinary(int intN) {
 
 void fnDecimalToOctal(int intN) {
     //==============OCTAL CONVERSION================
-    std::vector<int>nums; // list that will hold binary values
+    std::vector<int> vecOctal; // list that will hold binary values
 
 
     int i = 0; // iterator for vector
@@ -62,16 +65,16 @@ void fnDecimalToOctal(int intN) {
     while (intN != 0)
     {
         //Similar approach with that of octal but instead we deal with base 8.
-        nums.push_back(intN % 8); // adds binary value to the back of string 
+        vecOctal.push_back(intN % 8); // adds binary value to the back of string 
         i++; // i gets incremented for the next position in vector 
         intN = intN / 8;
 
     }
 
-    reverse(nums.begin(), nums.end()); // reverses order of vector 
+    reverse(vecOctal.begin(), vecOctal.end()); // reverses order of vector 
 
-    for (int i = 0; i < nums.size(); i++) {
-        std::cout << nums[i];
+    for (int i = 0; i < vecOctal.size(); i++) {
+        std::cout << vecOctal[i];
     }
 
 
@@ -82,7 +85,7 @@ void fnDecimalToOctal(int intN) {
 
 void fnDecimalToHexadecimal(int intN) {
     //==============HEXADECIMAL CONVERSION================
-    std::vector<int>nums; // list that will hold binary values
+    std::vector<int>vecBinary; // list that will hold binary values
 
 
     int i = 0; // iterator for vector
@@ -91,18 +94,171 @@ void fnDecimalToHexadecimal(int intN) {
     while (intN != 0)
     {
         //Similar approach with that of octal but instead we deal with base 8.
-        nums.push_back(intN % 16); // adds binary value to the back of string 
+        vecBinary.push_back(intN % 2); // adds binary value to the back of string 
         i++; // i gets incremented for the next position in vector 
-        intN = intN / 16;
+        intN = intN / 2;
 
     }
 
-    reverse(nums.begin(), nums.end()); // reverses order of vector 
+    reverse(vecBinary.begin(), vecBinary.end()); // reverses order of vector 
 
-    for (int i = 0; i < nums.size(); i++) {
-        std::cout << nums[i];
+    std::string temp = "";
+
+    //This checks to see if the binary number can be equally seperated into 4 bits. 
+    //In some cases, we may have 1 1111 or 11 1111 or 111 1111 but need 0001 1111, 0011 1111, 0111 1111. This solves that problem.
+    //Note that we also have a variable j. j will be used in the following for loop. 
+    int j = 0;
+    if ( (vecBinary.size() + 1) % 4 == 0) {
+        temp = "0";
+        j = 1;
+        //std::cout << "\ntemp max size + 1 is true\n\n";
+    }
+    else if ( (vecBinary.size() + 2) % 4 == 0) {
+        temp = "00";
+        j = 2;
+        //std::cout << "\ntemp max size + 2 is true\n\n";
+    }
+    else if ( (vecBinary.size() + 3) % 4 == 0) {
+        temp = "000";
+        j = 3;
+        //std::cout << "\ntemp max size + 3 is true\n\n";
     }
 
+
+    //If Binary string is MORE than length 4
+    if (vecBinary.size() > 4) {
+        //Loops through the the vector containing binary string.
+        for (int i = 0; i < vecBinary.size(); i++) {
+
+            //If j is 0. J may already be three to indicate for the given 0s that were not previously added if it were 1 1111 for example. Or it may be 2 or 1. 
+            //When J = 0 though, we know that the entire bit string is equally splitted into 4 parts.
+            //Thus, we have to be sure that we concatonate whatever is in num[0] to temp, in which temp is empty.
+            if (j == 0) {
+                temp = temp + std::to_string(vecBinary[i]);
+                j = j + 1;
+            }
+            //J % 4 indicates that we have a 4 bit string. We want to the 4 bt string to a temp vector so that we can then 
+            //convert that string into a hexadecimal later. 
+            else if ((j % 4 == 0) && (j != 0)) {
+                temp = "" + std::to_string(vecBinary[i]);
+                //Resets j = 0 since we have already looped through a full length of 4 bit string. Or 4 characters.
+                j = 0;
+            }
+            //If j is 5, 6, 7, 9, 10, 11, 13, etc., as long as j%4 != 0. 
+            else {
+                temp = temp + std::to_string(vecBinary[i]);
+                if (temp == "0000") {
+                    std::cout << "0";
+                }
+                else if (temp == "0001") {
+                    std::cout << "1";
+                }
+                else if (temp == "0010") {
+                    std::cout << "2";
+                }
+                else if (temp == "0011") {
+                    std::cout << "3";
+                }
+                else if (temp == "0100") {
+                    std::cout << "4";
+                }
+                else if (temp == "0101") {
+                    std::cout << "5";
+                }
+                else if (temp == "0110") {
+                    std::cout << "6";
+                }
+                else if (temp == "0111") {
+                    std::cout << "7";
+                }
+                else if (temp == "1000") {
+                    std::cout << "8";
+                }
+                else if (temp == "1001") {
+                    std::cout << "9";
+                }
+                else if (temp == "1010") {
+                    std::cout << "A";
+                }
+                else if (temp == "1011") {
+                    std::cout << "B";
+                }
+                else if (temp == "1100") {
+                    std::cout << "C";
+                }
+                else if (temp == "1101") {
+                    std::cout << "D";
+                }
+                else if (temp == "1110") {
+                    std::cout << "E";
+                }
+                else if (temp == "1111") {
+                    std::cout << "F";
+                }
+                //std::cout << "Branch 3 is True. Thus, temp is: " << temp;
+                //std::cout << std::endl << std::endl;
+                j = j + 1;
+
+            }
+            //std::cout << std::endl;
+        }
+    }
+
+    //If binary string is ONLY of length 4
+    else {
+        for (int i = 0; i < vecBinary.size(); i++) {
+            temp = temp + std::to_string(vecBinary[i]);
+
+        }
+        if (temp == "0000") {
+            std::cout << "0";
+        }
+        else if (temp == "0001") {
+            std::cout << "1";
+        }
+        else if (temp == "0010") {
+            std::cout << "2";
+        }
+        else if (temp == "0011") {
+            std::cout << "3";
+        }
+        else if (temp == "0100") {
+            std::cout << "4";
+        }
+        else if (temp == "0101") {
+            std::cout << "5";
+        }
+        else if (temp == "0110") {
+            std::cout << "6";
+        }
+        else if (temp == "0111") {
+            std::cout << "7";
+        }
+        else if (temp == "1000") {
+            std::cout << "8";
+        }
+        else if (temp == "1001") {
+            std::cout << "9";
+        }
+        else if (temp == "1010") {
+            std::cout << "A";
+        }
+        else if (temp == "1011") {
+            std::cout << "B";
+        }
+        else if (temp == "1100") {
+            std::cout << "C";
+        }
+        else if (temp == "1101") {
+            std::cout << "D";
+        }
+        else if (temp == "1110") {
+            std::cout << "E";
+        }
+        else if (temp == "1111") {
+            std::cout << "F";
+        }
+    }
 
     std::cout << std::endl;
     //==============HEXADECIMAL CONVERSION================
@@ -111,6 +267,7 @@ void fnDecimalToHexadecimal(int intN) {
 
 int main()
 {
+    std::vector<int>vecBinaryVal; // list that will hold binary values
     int intN;
     bool boolStayOrQuit = true;
 
@@ -121,6 +278,8 @@ int main()
         std::cout << "This program will convert a given integer into its binary, octal, and hexadecimal value.\n\nEnter Number here: ";
         std::cin >> intN; // number input 
         std::cout << std::endl;
+
+        //Find a way to make fnDecimalToBinary into a int function that returns an int. 
 
         std::cout << "Binary: ";
         fnDecimalToBinary(intN);
